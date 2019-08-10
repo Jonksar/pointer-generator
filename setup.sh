7d77798677c854a4b9955233659bf643772f258e
Joonatan Samuel <jonksar@github.com>
2019-08-10 23:54:11 +0300

    First ever version of setup.sh, untested

---
+++ setup.sh
@@ -0,0 +1,16 @@
+function gdrive_download () {
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  rm -rf /tmp/cookies.txt
+}
+
+# Download the CNN DailyMail preprocessed dataset
+# Source: https://github.com/JafferWilson/Process-Data-of-CNN-DailyMail
+# WARNING: Approx 1 GB
+gdrive_download "0BzQ6rtO2VN95a0c3TlZCWkl3aU0" "finished_files.zip"
+unzip "finished_files.zip"
+
+# Download the pretrained model weights
+# Source: From README in https://github.com/Jonksar/pointer-generator
+gdrive_download "0B7pQmm-OfDv7ZUhHZm9ZWEZidDg" "pretrained_model_tf1.2.1.zip"
+unzip "pretrained_model_tf1.2.1.zip"
